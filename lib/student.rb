6c7b2f41c48159462efc96725e8fcfdb262d9fab
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-19 19:41:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ end
     end.first
   end
 
-  def self.first_x_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
